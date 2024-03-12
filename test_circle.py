de8f627a8f0990a4c8c592f5e7857ffc4da69884
Nicha RUANGRIT <nicha.rua@ku.th>
2024-03-12 16:58:20 +0700

    removed comment line

--- test_circle.py
+++ test_circle.py
@@ -11,13 +11,8 @@ from circle import Circle
 import unittest
 import math
 
-# TODO write 3 tests as described above
 
 class CircleTest(unittest.TestCase):
-
-    # def setUp(self):
-    #     self.circle = Circle(0)
-
     def test_positive_radius(self):
         """Test add_area with two circles having positive radii."""
         c1 = Circle(3)
@@ -28,8 +23,7 @@ class CircleTest(unittest.TestCase):
 
     def test_radius_zero(self):
         """Test add_area when one of the circles has radius 0,
-         the other has non-zero radius. (Result should have same radius.)
-         Does add_area work the same regardless of which circle has radius 0?"""
+         the other has non-zero radius. (Result should have same radius.)"""
         c1 = Circle(0)
         c2 = Circle(5)
         c3 = c1.add_area(c2)
