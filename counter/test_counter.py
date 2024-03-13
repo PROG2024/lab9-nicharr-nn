cf550bc3f8e7eebbf55bc624e7fa93e592db36e0
Nicha RUANGRIT <nicha.rua@ku.th>
2024-03-13 14:07:46 +0700

    fixed test

--- counter/test_counter.py
+++ counter/test_counter.py
@@ -23,8 +23,8 @@ class TestCounter(unittest.TestCase):
         c1 = Counter()
         c2 = Counter()
         c1.increment()
-        self.assertEqual(c1.count, 1)
-        self.assertEqual(c2.count, 1)
+        self.assertEqual(c1.count, 2)
+        self.assertEqual(c2.count, 2)
 
     def test_no_reset(self):
         """Test is not reset to 0 when you invoke count = Counter() after the first time."""
